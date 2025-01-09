1096dc08f39fb15216436e0df855ca803cdad85f
Bhagirath Bhatti <66365538+bhagirath1312@users.noreply.github.com>
2025-01-10 04:23:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-app.use(cors({ origin: 'https://personal-website-1-en50.onrender.com/' }));
+app.use(cors({ origin: 'https://personal-website-1-en50.onrender.com' }));
 app.use(express.json()); 
 
 app.post('/subscribe', (req, res) => {
